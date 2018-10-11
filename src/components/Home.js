fc0dcd6b75cc3c0555f096bb42e69b7adce3f0f1
Max Dryer <mjdryer@bsu.edu>
2018-10-11 18:03:24 -0400

    Project setup, need to npm install many things

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import { withRouter, Switch, Route } from "react-router";
+
+
+class Home extends Component {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+}
+
+export default withRouter(Home)
---
+++ src/components/Results.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import { withRouter, Switch, Route } from "react-router";
+
+
+class Results extends Component {
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+}
+
+export default withRouter(Results)
