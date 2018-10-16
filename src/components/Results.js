ba29546de9f7fd5302adb77a7f2689a0cc5a5e4e
Tucker Miller <tdmiller7@bsu.edu>
2018-10-16 18:17:42 -0400

    Add Results page html
    Remove .idea/workspace.xml and .idea/oneamerica-insurance-calculator.iml

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import { withRouter, Switch, Route } from "react-router";
+import { withRouter } from "react-router";
 
 
 class Results extends Component {
@@ -7,7 +7,48 @@ class Results extends Component {
     render() {
         return (
             <div>
-                <h1>This is the Results page</h1>
+
+                <div className="overview">
+                    <h1>Lorem Ipsum Dolor Sit Amet, Consectetur Elit?</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                </div>
+
+                <div className="results-information">
+                    <h1>Generic Information You May Need</h1>
+                    <ol>
+                        <li>You might want to know the number $10,000</li>
+                        <li>You might also want to know the number of $12,000 for your spouse.</li>
+                        <br/>
+                        <li>You might want to know the number $10,000</li>
+                        <li>You might also want to know the number of $12,000 for your spouse.</li>
+                        <br/>
+                        <li>You might want to know the number $10,000</li>
+                        <li>You might also want to know the number of $12,000 for your spouse.</li>
+                    </ol>
+                </div>
+
+                <div>
+                    <form onSubmit={(e) => {
+                        e.preventDefault();
+                        this.submitInformation()
+                    }}/>
+
+                    <div className="customer-submit">
+
+                        <label for="full-name">Full Name</label>
+                        <input name="full-name" type="text" />
+
+                        <label for="email">Email</label>
+                        <input name="email" type="email" />
+
+                        <label for="phone">Phone</label>
+                        <input name="phone" type="tel" />
+
+                    </div>
+
+                    <button type="submit">Contact</button>
+
+                </div>
             </div>
         )
     }
