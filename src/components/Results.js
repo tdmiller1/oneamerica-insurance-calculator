25fb2f85d71d5a5223c3344362c05827b86da534
Tucker Miller <tdmiller7@bsu.edu>
2018-10-22 22:22:10 -0400

    Add first draft css to Results page

--- src/components/Results.js
+++ src/components/Results.js
@@ -7,25 +7,26 @@ class Results extends Component {
 
     render() {
         return (
-            <div className="component">
-
-                <div className="overview">
+            <div className="component center">
+                <div className="overview text">
                     <h1>Lorem Ipsum Dolor Sit Amet, Consectetur Elit?</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
                 </div>
 
                 <div className="results-information">
                     <h1>Generic Information You May Need</h1>
-                    <ol>
-                        <li>You might want to know the number $10,000</li>
-                        <li>You might also want to know the number of $12,000 for your spouse.</li>
-                        <br/>
-                        <li>You might want to know the number $10,000</li>
-                        <li>You might also want to know the number of $12,000 for your spouse.</li>
-                        <br/>
-                        <li>You might want to know the number $10,000</li>
-                        <li>You might also want to know the number of $12,000 for your spouse.</li>
-                    </ol>
+                    <div className="center">
+                        <ol>
+                            <li>You might want to know the number $10,000</li>
+                            <li>You might also want to know the number of $12,000 for your spouse.</li>
+                            <br/>
+                            <li>You might want to know the number $10,000</li>
+                            <li>You might also want to know the number of $12,000 for your spouse.</li>
+                            <br/>
+                            <li>You might want to know the number $10,000</li>
+                            <li>You might also want to know the number of $12,000 for your spouse.</li>
+                        </ol>
+                    </div>
                 </div>
 
                 <div>
@@ -53,6 +54,7 @@ class Results extends Component {
                             <br/>
                             <input name="phone" type="tel" />
                         </div>
+
                     </div>
 
                     <div className="customer-submit desktop">
@@ -65,10 +67,12 @@ class Results extends Component {
 
                         <label htmlFor="phone">Phone</label>
                         <input name="phone" type="tel"/>
+
                     </div>
 
+                    <button className="button" type="submit">Contact Us</button>
+
                 </div>
-                <button type="submit">Contact</button>
 
             </div>
         )
