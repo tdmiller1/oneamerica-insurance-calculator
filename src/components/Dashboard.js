fd3f37eed88bd406496dcc53000d92ce7f80fc09
Tucker Miller <tdmiller7@bsu.edu>
2019-01-22 12:13:33 -0500

    Add CliientCard temporary component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import { withRouter } from "react-router";
 import PropTypes from "prop-types";
+import ClientCard from "./ClientCard";
 
 class Dashbard extends Component {
 
@@ -9,29 +10,28 @@ class Dashbard extends Component {
     render() {
         
         return (
-            <div className="temporary-site-wrapper">
-                <div id="buffer">
-                    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"></link>
-                    <div class="w3-sidebar w3-light-grey w3-bar-block" className="width25">
-                        <h3 class="w3-bar-item">Filters</h3>
-                        <a href="#" class="w3-bar-item w3-button">Link 1</a><br></br>
-                        <a href="#" class="w3-bar-item w3-button">Link 2</a><br></br>
-                        <a href="#" class="w3-bar-item w3-button">Link 3</a><br></br>
+             <div>
+                
+                <div>
+                    <h3>Filters</h3>
+                    <a href="#" >Link 1</a><br></br>
+                    <a href="#" >Link 2</a><br></br>
+                    <a href="#" >Link 3</a><br></br>
+                </div>
+
+                <div>
+
+                    <div>
+                        <h1>Dashboard</h1>
                     </div>
 
-                    <div className="marginL25">
+                    <ClientCard name="Timmy" phone="555-555-5555"></ClientCard>
+                    <ClientCard name="Debby" phone="555-555-5555"></ClientCard>
+                    <ClientCard name="Bobby" phone="555-555-5555"></ClientCard>
+                    <ClientCard name="Davey" phone="555-555-5555"></ClientCard>
+                    <ClientCard name="Joshy" phone="555-555-5555"></ClientCard>
 
-                        <div class="w3-container">
-                            <h1>My Page</h1>
-                        </div>
 
-                        <div class="w3-container">
-                            <h2>Sidebar Navigation Example</h2>
-                            <p>The sidebar with is set with "style="width:25%".</p>
-                            <p>The left margin of the page content is set to the same value.</p>
-                        </div>
-
-                    </div>
                 </div>
             </div>
         )
